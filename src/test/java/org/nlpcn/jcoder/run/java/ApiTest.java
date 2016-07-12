7311c83a77a4cb110b86fbc599613c670fd73fed
ansj <ansj-sun@163.com>
2016-07-12 12:08:11 +0800

    update

--- src/test/java/org/nlpcn/jcoder/run/java/ApiTest.java
+++ src/test/java/org/nlpcn/jcoder/run/java/ApiTest.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.nlpcn.jcoder.run.annotation.Cache;
 import org.nlpcn.jcoder.run.annotation.DefaultExecute;
 import org.nlpcn.jcoder.run.annotation.Single;
+import org.nlpcn.jcoder.util.Restful;
 import org.nutz.ioc.loader.annotation.Inject;
 
 /**
@@ -16,24 +17,19 @@ import org.nutz.ioc.loader.annotation.Inject;
 @Single(true)
 public class ApiTest {
 	
-
 	@Inject
 	private Logger log;
-	Integer a = 0  ;
 
 	/**
 	 * 输入姓名返回ok字符串
 	 * @param name 输入的名字
-	 * @return 
-	 * @return 结果信息
+	 * @return {"message":"hello jcoder! ","ok":true}
 	 * @throws InterruptedException 线程停止时抛出
 	 */
 	@DefaultExecute
 	@Cache
-	public Integer defaultTest(HttpServletRequest req) throws InterruptedException {
-		System.out.println(req);
-		a++ ;
-		return a ;
+	public Object defaultTest(HttpServletRequest req,String name) throws InterruptedException {
+		return Restful.instance("hello jcoder! ", null) ;
 	}
 
 }
