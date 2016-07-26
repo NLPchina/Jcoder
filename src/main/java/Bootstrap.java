5190eca0481da10df88053cf8156d9baf9ffd8c8
ansj <ansj-sun@163.com>
2016-07-26 14:00:36 +0800

    rm httputil rm jsoup add maven config to project

--- src/main/java/Bootstrap.java
+++ src/main/java/Bootstrap.java
@@ -180,11 +180,14 @@ public class Bootstrap {
 		if (!libDir.exists()) {
 			libDir.mkdirs();
 			wirteFile(new File(libDir, "pom.xml").getAbsolutePath(), "utf-8",
-					"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+					"<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
 							+ "	xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" + "	<modelVersion>4.0.0</modelVersion>\n"
-							+ "	<groupId>org.nlpcn</groupId>\n" + "	<artifactId>jcoder</artifactId>\n" + "	<version>1.2</version>\n" + "	<description>use maven to down jars</description>\n"
-							+ "  \n" + "	<dependencies>\n" + "\n" + "	</dependencies>\n" + "\n" + "	<build>  \n" + "		<defaultGoal>compile</defaultGoal>\n" + "	</build>\n"
-							+ "</project>");
+							+ "	<groupId>org.nlpcn</groupId>\n" + "	<artifactId>jcoder</artifactId>\n" + "	<version>0.1</version>\n" + "	\n" + "	<dependencies>\n" + "	</dependencies>\n" + "\n"
+							+ "	<build>\n" + "		<sourceDirectory>src/main/java</sourceDirectory>\n" + "		<testSourceDirectory>src/test/java</testSourceDirectory>\n" + "		\n"
+							+ "		<plugins>\n" + "			<plugin>\n" + "				<artifactId>maven-compiler-plugin</artifactId>\n" + "				<version>3.3</version>\n"
+							+ "				<configuration>\n" + "					<source>1.8</source>\n" + "					<target>1.8</target>\n"
+							+ "					<encoding>UTF-8</encoding>\n" + "					<compilerArguments>\n" + "						<extdirs>lib</extdirs>\n"
+							+ "					</compilerArguments>\n"+"				</configuration>\n" + "			</plugin>\n" + "		</plugins>\n" + "	</build>\n" + "</project>\n" + "");
 		}
 
 		File tmpDir = new File(JcoderHome, "tmp"); // create tmp dir
@@ -247,7 +250,7 @@ public class Bootstrap {
 			key = PREFIX + key;
 		}
 		if (value != null) {
-			System.out.println("put env to system.propertie: "+key+" ============= "+value);
+			System.out.println("put env to system.propertie: " + key + " ============= " + value);
 			System.setProperty(key, value);
 		}
 	}
