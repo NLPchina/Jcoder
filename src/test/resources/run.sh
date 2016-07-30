25738f4b6e0a2daddc7bdfdbd3bcdcbc5396251c
ansj <ansj-sun@163.com>
2016-07-30 10:55:59 +0800

    add script by start

---
+++ src/test/resources/run.sh
@@ -0,0 +1,24 @@
+start()
+{
+  nohup java -jar jcoder-1.6.war -f=jcoder.conf &
+  echo $! >jcoder.pid
+}
+stop()
+{
+  kill  `cat jcoder..pid`
+}
+
+
+case $1 in
+"restart")
+   stop
+   start
+;;
+"start")
+   start
+;;
+"stop")
+   stop
+;;
+*) echo "only accept params start|stop|restart" ;
+esac
