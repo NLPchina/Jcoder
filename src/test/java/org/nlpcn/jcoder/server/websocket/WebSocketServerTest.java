154db1ec45733f30d4badba889370341f70385fc
孙健 <sunj@infcn.com.cn>
2017-01-18 15:25:21 +0800

    up

--- src/test/java/org/nlpcn/jcoder/server/websocket/WebSocketServerTest.java
+++ src/test/java/org/nlpcn/jcoder/server/websocket/WebSocketServerTest.java
@@ -1,6 +1,7 @@
 package org.nlpcn.jcoder.server.websocket;
 
 import org.junit.Test;
+import org.nlpcn.jcoder.server.rpc.ChannelManager;
 import org.nlpcn.jcoder.server.rpc.websocket.WebSocketServer;
 
 public class WebSocketServerTest {
@@ -8,6 +9,7 @@ public class WebSocketServerTest {
 	@Test
 	public void test() throws Exception {
 		WebSocketServer.startServer(8081);
+		Thread.sleep(1000000);
 	}
 
 }
