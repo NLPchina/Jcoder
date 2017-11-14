d5710cadd274f9a8a09e6f8ecdc945b2c4d86960
孙健 <sunj@infcn.com.cn>
2017-11-14 11:02:13 +0800

    add use define token key

--- src/main/java/org/nlpcn/jcoder/service/TokenService.java
+++ src/main/java/org/nlpcn/jcoder/service/TokenService.java
@@ -17,39 +17,51 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class TokenService {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(TokenService.class) ;
 
+	private static final Logger LOG = LoggerFactory.getLogger(TokenService.class);
 
 	public static Token getToken(String key) throws ExecutionException {
-		return SharedSpace.getToken(key) ;
+		return SharedSpace.getToken(key);
 	}
-	
+
 	/**
 	 * regeidt a token by user
+	 * 
 	 * @param user
 	 * @return
-	 * @throws ExecutionException 
+	 * @throws ExecutionException
 	 */
-	public static String regToken(User user) throws ExecutionException{
-		LOG.info(user.getName()+" to create a key");
-		String key = UUID.randomUUID().toString();
+	public static String regToken(User user) throws ExecutionException {
+		return regToken(user, UUID.randomUUID().toString());
+	}
+
+	/**
+	 * regeidt a token by user
+	 * 
+	 * @param user
+	 * @param key ,用户自定义自己的token，用户自己保持不重复
+	 * @return
+	 * @throws ExecutionException
+	 */
+	public static String regToken(User user, String key) throws ExecutionException {
+		LOG.info(user.getName() + " to create a key");
 		Token token = new Token();
 		token.setToken(key);
 		token.setCreateTime(new Date());
 		token.setUser(user);
-		SharedSpace.regToken(token) ;
+		SharedSpace.regToken(token);
 		return token.getToken();
 	}
-	
+
 	/**
 	 * login out by token
+	 * 
 	 * @param key
 	 * @return
 	 */
-	public static Token removeToken(String key){
-		Token token = SharedSpace.removeToken(key) ;
-		LOG.info(token+" to create a key");
-		return token ;
+	public static Token removeToken(String key) {
+		Token token = SharedSpace.removeToken(key);
+		LOG.info(token + " to create a key");
+		return token;
 	}
 }
