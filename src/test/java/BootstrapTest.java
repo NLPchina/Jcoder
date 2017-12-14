82b6349a86605ca2577a34eea651adb541353e40
Ansj <sunj@infcn.com.cn>
2017-12-14 15:43:05 +0800

    fix left

--- src/test/java/BootstrapTest.java
+++ src/test/java/BootstrapTest.java
@@ -2,7 +2,7 @@ public class BootstrapTest {
 	public static void main(String[] args) throws Exception {
 		Bootstrap.main(new String[]{
 				"--zk=192.168.3.137:2181",
-				"--host=192.168.3.119",
+				"--host=192.168.3.66",
 				"--home=jcoder_home"
 		});
 	}
