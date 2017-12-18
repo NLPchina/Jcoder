c9350a227c5b1fbaa3935d09662fee5668fb0495
hanx <hanx1993>
2017-12-18 16:26:29 +0800

    modify

--- src/main/java/org/nlpcn/jcoder/service/IocService.java
+++ src/main/java/org/nlpcn/jcoder/service/IocService.java
@@ -4,20 +4,21 @@ package org.nlpcn.jcoder.service;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.nlpcn.jcoder.util.StaticValue;
 import org.nutz.ioc.loader.annotation.IocBean;
 
 @IocBean
 public class IocService {
 	private SharedSpaceService sharedSpaceService;
 
-	public IocService(SharedSpaceService sharedSpaceService) {
-		this.sharedSpaceService = sharedSpaceService;
+	public IocService() {
+		this.sharedSpaceService = StaticValue.space();
 	}
 	
 	public Set<String> getAllHosts(final String groupName) throws Exception {
 		return sharedSpaceService.getHostGroupCache().keySet().stream().filter(s -> {
 			String[] split = s.split("_"); 
-			return groupName.equals(split[2]) ;
+			return groupName.equals(split[1]) ;
 		}).collect(Collectors.toSet());
 	}
 }
