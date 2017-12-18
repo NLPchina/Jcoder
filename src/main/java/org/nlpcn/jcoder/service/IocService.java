5fb64a1ac5378735521c0753fa16a117cee21b08
Ansj <sunj@infcn.com.cn>
2017-12-18 14:50:53 +0800

    up

--- src/main/java/org/nlpcn/jcoder/service/IocService.java
+++ src/main/java/org/nlpcn/jcoder/service/IocService.java
@@ -1,9 +1,12 @@
 package org.nlpcn.jcoder.service;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.nlpcn.jcoder.domain.HostGroup;
 import org.nutz.ioc.loader.annotation.IocBean;
 
 @IocBean
@@ -14,10 +17,19 @@ public class IocService {
 		this.sharedSpaceService = sharedSpaceService;
 	}
 	
-	public Set<String> getAllHosts(final String groupName) throws Exception {
-		return sharedSpaceService.getHostGroupCache().keySet().stream().filter(s -> {
-			String[] split = s.split("_"); 
-			return groupName.equals(split[2]) ;
-		}).collect(Collectors.toSet());
+	public Map<String,HostGroup> getAllHosts(final String groupName) throws Exception {
+
+		Map<String,HostGroup> result = new HashMap<>() ;
+
+		Set<Map.Entry<String, HostGroup>> entries = sharedSpaceService.getHostGroupCache().entrySet();
+
+		for (Map.Entry<String, HostGroup> entry : entries) {
+			String[] split = entry.getKey().split("_");
+
+			if(split[1].equals(groupName)){
+				result.put(split[0],entry.getValue()) ;
+			}
+		}
+		return result ;
 	}
 }
