ac8892ac42c497686b48eb4fbef106a936f3ffc0
hanx <hanx1993>
2018-02-28 15:43:22 +0800

    日志模块完善

--- src/main/webapp/modules/logs/main.js
+++ src/main/webapp/modules/logs/main.js
@@ -1,54 +1,18 @@
-var logsManager = new Vue({
+vmApp.module = new Vue({
   el: '#logsManager',
   data: {
     fileInfo:null,
     hosts:[],
     editor: '',
     groups:[],
-    webSockets:[],
+    webSockets:{},
     checkedHosts:[],
-    activeGroup:'',
     ok:true
   },
   mounted:function(){
 	  var $this = this;
 	  $this.getHosts();
 	  $this.getGroups();
-
-
-
-	  /*$this.editor = CodeMirror.fromTextArea(document.getElementById('logsInfoConsole'), {
-         lineNumbers : true,
-         mode : "xml",
-         matchBrackets : true,
-         theme : "monokai",
-         showCursorWhenSelecting:true
-      });*/
-      $this.checkedHosts = $this.hosts;
-  },
-  watch:{
-    'checkedHosts':function(val){
-        
-        for(var i = 0; i < this.hosts.length; i++){
-            var flag = true;
-            for(var a = 0; a < this.checkedHosts.length; a++){
-                if(this.hosts[i] == this.checkedHosts[a]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                // 首先,需要创建一个WebSocket连接
-                var ws = new WebSocket("ws://"+this.hosts[i]+"/log");
-                console.log('close');
-                ws.onclose = function (event) {
-                    console.log("websocket onmessage", event.data);
-                };
-                continue;
-            }
-        }
-        this.initLogs(this.activeGroup);
-    }
   },
   methods:{
       getHosts:function(){
@@ -60,47 +24,52 @@ var logsManager = new Vue({
              for(var i = 0;i < data.obj.length; i++){
                 $this.hosts.push(data.obj[i]);
              }
+             $this.checkedHosts = $this.hosts;
+             Vue.nextTick(function(){
+              for(var i = 0;i < $this.checkedHosts.length;i++){
+                  $(":checkbox").each(function(index){
+                      if($(this).val() == $this.checkedHosts[i])$(this).attr('checked',true);
+                  });
+               }
+               $this.initLogs('_jcoder_log');
+             });
           }else{
              JqdeBox.message(false, "主机列表获取失败！");
           }
         });
       },
       getGroups:function(){
-          //JqdeBox.loading();
           var $this = this;
           Jcoder.ajax('/admin/logs/getAllGroups', 'post',null,null).then(function (data) {
-            //JqdeBox.unloading();
             if(data.ok){
                for(var i = 0;i < data.obj.length; i++){
                   $this.groups.push(data.obj[i]);
                }
-               Vue.nextTick(function(){
-                    $this.getGroupLogInfo($this.groups[0]);
-               });
             }else{
                JqdeBox.message(false, "group列表获取失败！");
             }
           });
       },
+      //初始化的时候执行一次
       initLogs:function(group){
-        console.log(group);
+        $('#groups ul li').eq(0).children().eq(0).addClass('infobox-green');
         var me = this;
         for(var i = 0; i < me.checkedHosts.length; i++){
             // 首先,需要创建一个WebSocket连接
             var ws = new WebSocket("ws://"+me.checkedHosts[i]+"/log");
-            me.webSockets.push(ws);
+            var key = me.checkedHosts[i]+"";
+            me.webSockets[key] = ws;
+            console.log(me.webSockets);
             // 连接成功后,会触发onopen回调
             ws.onopen = function(event) {
-                console.log("websocket onopen ...");
                 // 加入home房间
-                console.log(group);
-                ws.send(JSON.stringify({groupName:group,methodName:"join"}));//'className':group,'methodName':"join"{groupName:group,methodName:"join"}
+                console.log('open....');
+                ws.send(JSON.stringify({'groupName':group,'methodName':"join"}));
             };
             // 收到服务器发来的信息时触发的回调
             ws.onmessage = function(event) {
-                console.log("websocket onmessage", event.data);
-                $("#logsInfoConsole").append("<p>aaaaa</p>");
-                $("#logsInfoConsole").append("<p>"+event.data+"</p>");
+                console.log(event.data);
+                $("#logsInfoConsole").append("<p>"+JSON.parse(event.data).message+"</p>");
             };
         }
         //setInterval(me.ws_ping(), 25000); // 25秒一次就可以了
@@ -113,18 +82,62 @@ var logsManager = new Vue({
            	}
         }
       },
+      //获取指定group日志信息
       getGroupLogInfo:function(groupName){
-         var $this = this;
+        var $this = this;
         $("#groups ul li").each(function(index){
-            if(groupName == $(this).prop('innerText').replace("\n","")){
-                $('.infobox-green').removeClass('infobox-green');
-                $(this).children().eq(0).addClass('infobox-green');
+            var currentGroup = $(this).children().eq(0);
+            if(groupName == currentGroup.prop('id').replace("\n","")){
+                if(currentGroup.hasClass('infobox-green')){
+                    $(this).children().eq(0).removeClass('infobox-green');
+                    $this.openOrCloseGroupLog(groupName,'left');
+                }else{
+                    $(this).children().eq(0).addClass('infobox-green');
+                    $this.openOrCloseGroupLog(groupName,'join');
+                }
                 return false;
             }
         });
-        $this.activeGroup = groupName;
-        $this.initLogs($this.activeGroup);
+      },
+      //开启指定group日志监控
+      openOrCloseGroupLog:function(group,type){
+        var me = this;
+        for(var i = 0; i < me.checkedHosts.length; i++){
+            //获取连接
+            var key = me.checkedHosts[i]+"";
+            var ws = me.webSockets[key];
+            //把连接中指定group开启或移除
+            ws.send(JSON.stringify({'groupName':group,'methodName':type}));
+        }
+      },
+      openOrCloseHost:function(host){
+        var me = this;
+        var key = host+'';
+        var ws = me.webSockets[key];
+        var type = '';
+        $(":checkbox").each(function(index){
+          if($(this).val() == host){
+            if($(this).is(":checked")){
+                type = 'join';
+            }else{
+                type = 'left';
+            }
+          }
+        });
+        $("#groups ul li").each(function(index){
+            var currentGroup = $(this).children().eq(0);
+            if(currentGroup.hasClass('infobox-green')){
+                ws.send(JSON.stringify({'groupName':currentGroup.prop('id'),'methodName':type}));
+            }
+            return false;
+        });
       }
-  }
+  },
+  beforeDestroy: function () {
+    var me = this;
+    for(var i = 0;i < me.webSockets.length;i++){
+        me.webSockets[i].close();
+    }
+  },
 });
 
